c6e27225872ce5bab69ef43baf7dbca25e3870dd
yitong86 <sophiajiang86@hotmail.com>
2023-03-14 14:59:15 -0500

    Initial commit

---
+++ src/main/java/com/perscholas/FirstProject/ExampleOne/ExampleOneApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.FirstProject.ExampleOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/FirstProject/ExampleOne/ExampleOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.FirstProject.ExampleOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
